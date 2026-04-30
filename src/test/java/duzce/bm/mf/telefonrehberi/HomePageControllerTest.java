ce235e02dd18a620339c43fb8c06dd0c095d6a04
umut1can2 <akyolumut01@hotmail.com>
2026-04-30 22:50:22 +0300

    Test src eklendi pom guncellendi.

---
+++ src/test/java/duzce/bm/mf/telefonrehberi/HomePageControllerTest.java
@@ -0,0 +1,55 @@
+package duzce.bm.mf.telefonrehberi;
+
+import duzce.bm.mf.telefonrehberi.config.AppConfig;
+import duzce.bm.mf.telefonrehberi.config.WebConfig;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {WebConfig.class, AppConfig.class})
+@WebAppConfiguration
+public class HomePageControllerTest {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private static MockMvc mockMvc;
+
+    @BeforeAll
+    public static void setup(@Autowired WebApplicationContext wac) {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(wac)
+                .build();
+    }
+
+    @Test
+    public void testHomePage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testHomePageWithDepartment() throws Exception {
+        mockMvc.perform(get("/")
+                        .param("departmentId", "1"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testHomePageWithSubDepartment() throws Exception {
+        mockMvc.perform(get("/")
+                        .param("subDepartmentId", "1"))
+                .andExpect(status().isOk());
+    }
+}
