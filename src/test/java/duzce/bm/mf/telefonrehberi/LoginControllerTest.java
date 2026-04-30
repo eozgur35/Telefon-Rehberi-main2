71f42ddd457ea8d73a1f20e31b4b763aa64c3a15
umut1can2 <akyolumut01@hotmail.com>
2026-04-30 23:31:15 +0300

    LoginControllerTest eklendi ve pom.xml hamcrest diye bir kutuphane eklendi.

---
+++ src/test/java/duzce/bm/mf/telefonrehberi/LoginControllerTest.java
@@ -0,0 +1,73 @@
+package duzce.bm.mf.telefonrehberi;
+
+import duzce.bm.mf.telefonrehberi.config.AppConfig;
+import duzce.bm.mf.telefonrehberi.config.WebConfig;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+// Statik importlar (get, post ve status için şart)
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {WebConfig.class, AppConfig.class})
+@WebAppConfiguration
+public class LoginControllerTest {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private static MockMvc mockMvc;
+
+    @BeforeAll
+    public static void setup(@Autowired WebApplicationContext wac) {
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+    }
+
+    // Login sayfasının yuklenmesini kontrol ediyor
+    @Test
+    public void testLoginPageLoad() throws Exception {
+        mockMvc.perform(get("/login"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("login"));
+    }
+
+    // Hatalı giriş denemesini test eder (Sayfa yine login döner)
+    @Test
+    public void testLoginFailure() throws Exception {
+        mockMvc.perform(post("/login")
+                        .param("email", "yanlis@duzce.edu.tr")
+                        .param("password", "123456"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("login"));
+    }
+
+
+    // dogru giris degerlerini guncelle!!!
+
+    @Test
+    public void testLoginSuccess() throws Exception {
+        mockMvc.perform(post("/login")
+                        .param("email", "admin@duzce.edu.tr")
+                        .param("password", "12345"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("login"));
+    }
+
+    // cikis islemini test ediyor
+    @Test
+    public void testLogoutRedirect() throws Exception {
+        mockMvc.perform(get("/logout"))
+                .andExpect(status().is3xxRedirection());
+    }
+}
