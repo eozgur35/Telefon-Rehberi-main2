a0a4fae46dc36b83ae9330fd8967567cf37a3b44
beyzayucel <beyzanuryucel66@gmail.com>
2026-05-06 01:14:46 +0300

    file utile taşındı

--- src/main/java/duzce/bm/mf/telefonrehberi/controller/AdminController.java
+++ src/main/java/duzce/bm/mf/telefonrehberi/controller/AdminController.java
@@ -16,10 +16,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile; // Dosya yüklemek için eklendi
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.Base64; // Base64 dönüşümü için eklendi
 import java.util.List;
 import java.util.Objects;
 
